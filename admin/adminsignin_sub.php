5813a7a422f5020bcb4086cb0b2b73470ee614c7
roshan-singh2020 <92023811+roshan-singh2020@users.noreply.github.com>
2021-12-06 22:08:17 +0545

    Add files via upload

---
+++ admin/adminsignin_sub.php
@@ -0,0 +1,13 @@
+<?php
+    include("class/users.php");
+    $signin=new users;
+    extract($_POST);
+    if($signin->signin($n,$p))
+    {
+        $signin->url("admin.php");
+    }
+    else
+    {
+        $signin->url("adminlogin.php?run=failed");
+    }
+?>
